c3914ffd5a2a4483c8e469bcaa5f76c7a260117b
teresa camarena <materesacamarena@gmail.com>
2017-06-27 00:40:14 -0700

    the employees managment page now looks like the one for manage stores

--- FiestaReports/Controllers/EmployeeController.cs
+++ FiestaReports/Controllers/EmployeeController.cs
@@ -9,6 +9,8 @@ namespace FiestaReports.Controllers
 {
     public class EmployeeController : Controller
     {
+        private FiestaZohoDatabaseEntities db = new FiestaZohoDatabaseEntities();
+
         // GET: Employee
         public ActionResult Index()
         {
@@ -17,8 +19,57 @@ namespace FiestaReports.Controllers
         
         public ActionResult ManageEmployee()
         {
-            return View();
+            Employee obj = new Employee();
+            obj.LoginUser = Convert.ToString(Session["UserName"]);
+            return View(obj);
         }
-      
+
+        /*******************************************************************
+            Author      : Gopi
+            Date        : 04/07/2017
+            Description : Returns Loggedin user Name
+           *******************************************************************/
+        [AuthorizationFilter]
+        public ActionResult RegisterEmployee()
+        {
+            Employee obj = new Employee();
+            obj.LoginUser = Convert.ToString(Session["UserName"]);
+            return View(obj);
+        }
+
+        [AuthorizationFilter]
+        [HttpGet]
+        public ActionResult GetAllEmployees()
+        {
+            var query = (from e in db.Fiesta_Employee
+                             join r in db.Fiesta_Role on e.RoleId equals r.RoleId
+                             join es in db.Fiesta_EmpStore on e.EmpId equals es.EmpId
+                             join s in db.Fiesta_Store on es.StoreNo equals s.StoreNo
+                             select new {
+                                 EmpId = e.EmpId,
+                                 FirstName = e.FirstName,
+                                 LastName = e.LastName,
+                                 Email = e.EmailAddress,
+                                 Role = r.RoleName,
+                                 Active = e.IsActive,
+                                 EmpStore = s.StoreNo
+                             }).ToList();
+
+            var employees = query.GroupBy(cc => cc.EmpId).Select(dd => new { EmpId = dd.Key, EmpStore = string.Join(",", dd.Select(ee => ee.EmpStore).ToList()) });
+
+            var other = (from e in employees
+                         join q in query on e.EmpId equals q.EmpId
+                         select new {
+                             FirstName = q.FirstName,
+                             LastName = q.LastName,
+                             Email = q.Email,
+                             Role = q.Role,
+                             EmpStore = e.EmpStore,
+                             Active = q.Active
+                         }).Distinct().ToList();
+
+
+            return Json(other, JsonRequestBehavior.AllowGet);
+        }
     }
 }
